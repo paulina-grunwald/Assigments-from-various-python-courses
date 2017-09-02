ae1679209fd7bdb5667798177cca37446a69d4fa
paulina-grunwald <paulina.grunwald@gmail.com>
2017-09-02 21:37:36 +0200

    upate project

---
+++ Learning Path Python Effective Data Analysis Using Python/twitter_data_accesss.py
@@ -0,0 +1,6 @@
+from tweepy import OAuthHandler
+
+consumer_key = 'W4vatXbYSgnTPhP3krd9Hle0Z'
+consumer_secret = 'cbMUSJiaHMvpo9y1Ql9DxCdRPY0hKILvtkZbiBY2QztsOMWSbh'
+access_token = '811617846396653568-WXcxBCS2r8stgHTttJ6egMO1om4x0ep'
+access_token_secret = 'lZuj4sBuQ3kTRi9yICJ1ohHREdbn1o3PJ8N01a7sZcLtg'
