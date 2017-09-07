f8a409e3bf1cf6d9b7f37c7ab5948f5b39121dcd
paulina-grunwald <paulina.grunwald@gmail.com>
2017-09-07 18:35:37 +0200

    update

--- Learning Path Python Effective Data Analysis Using Python/twitter_data_accesss.py
+++ Learning Path Python Effective Data Analysis Using Python/twitter_data_accesss.py
@@ -1,4 +1,4 @@
-import Json
+import json
 from tweepy import OAuthHandler, Stream, API
 from tweepy.streaming import StreamListener
 
@@ -14,11 +14,12 @@ auth.set_access_token(access_token, access_token_secret)
 
 class PrintListener(StreamListener):
     def on_status(self, status):
-        print(status.text)
-        print(status.author.screen_name,
-              status.created_at,
-              status.source,
-              '\n')
+        if not status.text[:3] == 'RT' :
+            print(status.text)
+            print(status.author.screen_name,
+                status.created_at,
+                status.source,
+                '\n')
     def on_error(self, status_code):
         print("Error code: {}".format(status_code))
         return True #keep stream alive
@@ -32,5 +33,14 @@ def print_to_terminal():
     language = ('ja','en') #language of the tweet
     stream.sample(languages=language)
 
+def pull_down_tweets(screen_name):
+    api = API(auth)
+    # count return number of tweets
+    tweets = api.user_timeline(screen_name=screen_name, count = 200)
+    #view tweets in json format
+    for tweet in tweets:
+        print(json.dumps(tweet._json, indent = 4))
+
 if __name__== '__main__':
-    print_to_terminal()
+    #print_to_terminal()
+    pull_down_tweets(auth.username)
