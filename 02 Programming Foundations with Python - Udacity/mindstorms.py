5c70fb458adc95b31266e64c6ad18a4dd1541211
paulina-grunwald <paulina.grunwald@gmail.com>
2017-09-16 21:43:39 +0200

    create two new files movies.py and mindstorms.py
    create new files in order to save more exercises

---
+++ 02 Programming Foundations with Python - Udacity/mindstorms.py
@@ -0,0 +1,14 @@
+#this program will draw a circle by drawing multiple squeres
+
+import turtle
+
+def draw_square():
+    
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+    window.exitonclick()
+
+draw_square()
