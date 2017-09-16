e96fb402359b5e445c5e4331fa9faccad3d367b4
paulina-grunwald <paulina.grunwald@gmail.com>
2017-09-16 21:01:59 +0200

    update course notes and prank.py code

--- 02 Programming Foundations with Python - Udacity/prank.py
+++ 02 Programming Foundations with Python - Udacity/prank.py
@@ -4,19 +4,21 @@ import os
 def rename_files():
     #find all the file names from the folder
     #os.listdir() lists all in the directory
-
     file_list = os.listdir(r"C:\Personal\GitHub\Python-course-assigments\02 Programming Foundations with Python - Udacity\prank")
     #change file names
     saved_path = os.getcwd()
     print("Current working drectory is"+saved_path)
+    #change directory
     os.chdir(r"C:\Personal\GitHub\Python-course-assigments\02 Programming Foundations with Python - Udacity\prank")
     print(file_list)
 
     #for each file, rename filename
     for file_name in file_list:
         print("Old Name - "+file_name)
-        print("New Name - "+file_name.translate(None, "0123456789"))
-        os.rename(file_name, file_name.translate(None, "0123456789"))
+        deletedigits = str.maketrans(dict.fromkeys("0123456789"))
+        os.rename(file_name, file_name.translate(deletedigits))
+        print("New Name - " + file_name)
     os.chdir(saved_path)
 
 rename_files()
+
